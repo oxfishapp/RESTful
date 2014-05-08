a61f04edf3c382ed8e29589d2303f735d316f662
Emerson <root@emerson-N53SN>
2014-05-08 13:05:57 -0500

    Inicialdo Proyecto desde Cero

---
+++ src/RESTful.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Apr 30, 2014
+
+@author: anroco
+'''
+
+from flask import Flask
+from flask import session
+from flask import app 
---
+++ src/timeline/app.py
@@ -0,0 +1,11 @@
+'''
+Created on May 8, 2014
+
+@author: root
+'''
+###
+#http://pycoder.net/bospy/presentation.html#api-structure
+###
+import flask
+
+#importando libreria flask
