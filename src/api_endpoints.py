c88f932b40775aaf90027702b061df2ec178fca7
Andres Rodriguez <anrocox@gmail.com>
2014-05-30 21:42:44 -0500

    Causa: modificar endpoints Efecto: edicion del nombre api por apiapplication en los endpoints

--- src/api_endpoints.py
+++ src/api_endpoints.py
@@ -18,28 +18,30 @@ endpoints = Blueprint('endpoints', __name__)
 
 api_version = '/api/1.0'
 
-api = restful.Api(endpoints)
+apiapplication = restful.Api(endpoints)
+
+
 
 #defir los diferentes endpoints de la aplicacion
-api.add_resource(views_users.User, api_version + '/user/')
-api.add_resource(views_users.Nickname, api_version + '/user/<string:nickname>')
-api.add_resource(views_users.User_scores, api_version + '/auth/user/')
-api.add_resource(views_users.Auth_user, api_version + '/login/')
-api.add_resource(views_users.User_register, api_version + '/auth/register/')
-api.add_resource(views_users.Generate_token, api_version + '/auth/get_token/')
+apiapplication.add_resource(views_users.User, api_version + '/user/')
+apiapplication.add_resource(views_users.Nickname, api_version + '/user/<string:nickname>')
+apiapplication.add_resource(views_users.User_scores, api_version + '/auth/user/')
+apiapplication.add_resource(views_users.Auth_user, api_version + '/login/')
+apiapplication.add_resource(views_users.User_register, api_version + '/auth/register/')
+apiapplication.add_resource(views_users.Generate_token, api_version + '/auth/get_token/')
 
-api.add_resource(views_timeline.Timeline_Index, api_version + '/publictimeline')
-api.add_resource(views_timeline.Timeline_Answers, api_version + '/allanswers')
-api.add_resource(views_timeline.Timeline_Home_Index, api_version + '/home/<string:key>')
-api.add_resource(views_timeline.Timeline_QandWinA, api_version + '/post_qwa/<string:key>')
-api.add_resource(views_timeline.Timeline_Update, api_version + '/auth/post_q', endpoint='post_q')
-api.add_resource(views_timeline.Timeline_Update, api_version + '/auth/post_a', endpoint='post_a')
-api.add_resource(views_timeline.Timeline_Update, api_version + '/auth/delete', endpoint='delete_q_a')
-api.add_resource(views_timeline.Timeline_Update, api_version + '/auth/update', endpoint='update_q')
+apiapplication.add_resource(views_timeline.Timeline_Index, api_version + '/publictimeline')
+apiapplication.add_resource(views_timeline.Timeline_Answers, api_version + '/allanswers')
+apiapplication.add_resource(views_timeline.Timeline_Home_Index, api_version + '/home/<string:key>')
+apiapplication.add_resource(views_timeline.Timeline_QandWinA, api_version + '/post_qwa/<string:key>')
+apiapplication.add_resource(views_timeline.Timeline_Update, api_version + '/auth/post_q', endpoint='post_q')
+apiapplication.add_resource(views_timeline.Timeline_Update, api_version + '/auth/post_a', endpoint='post_a')
+apiapplication.add_resource(views_timeline.Timeline_Update, api_version + '/auth/delete', endpoint='delete_q_a')
+apiapplication.add_resource(views_timeline.Timeline_Update, api_version + '/auth/update', endpoint='update_q')
 
-api.add_resource(views_skills.Skill_List, api_version + '/findbyskill/<string:skill>', endpoint='findbyskill')
-api.add_resource(views_skills.Skill_Update, api_version + '/skills' ,endpoint='updateskills')
-api.add_resource(views_skills.Skill_count, api_version + '/totalskills', endpoint='totalskills')
+apiapplication.add_resource(views_skills.Skill_List, api_version + '/findbyskill/<string:skill>', endpoint='findbyskill')
+apiapplication.add_resource(views_skills.Skill_Update, api_version + '/skills' ,endpoint='updateskills')
+apiapplication.add_resource(views_skills.Skill_count, api_version + '/totalskills', endpoint='totalskills')
 
 
 
